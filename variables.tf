a2f26cba9162dda3fa39e174822f2b0c22498b25
Ryan <ryan.fee625@gmail.com>
2020-01-27 10:55:58 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,10 +2,6 @@ variable "region" {}
 
 variable "vpc_id" {}
 
-variable "scalr_aws_secret_key" {}
-
-variable "scalr_aws_access_key" {}
-
 variable "sg_name" {
 description = "Security Group Name"
  }
